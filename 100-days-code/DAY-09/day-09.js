a353745b926fad5372fc46d52537416c52c9c7c0
Amir Sohel <amirsohel27@gmail.com>
2022-01-09 22:10:31 +0530

    solution commit for day-09

--- 100-days-code/DAY-09/day-09.js
+++ 100-days-code/DAY-09/day-09.js
@@ -5,10 +5,17 @@ const values = ["john", "canada", "pink", "734834"];
 const createObject01 = (keys, values) => {
   const finialObject = {};
   for (let key = 0; key < keys.length; key++) {
-    for (let value = 0; value < values.length; value++) {
+    for (let value = 0; value < values.length; value++)
       finialObject[keys[key]] = values[key];
-    }
   }
+  return finialObject;
+};
+
+// soluton 02
+const createObject02 = (keys, values) => {
+  const finialObject = {};
+  keys.forEach((key) => (finialObject[key] = ""));
+  values.forEach((value, index) => (finialObject[keys[index]] = value));
   return finialObject;
 };
 
