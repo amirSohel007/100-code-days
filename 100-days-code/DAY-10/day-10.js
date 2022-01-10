da9097b0ccc9720f8ae49fe107ce5725af077ca6
Amir Sohel <amirsohel27@gmail.com>
2022-01-10 23:50:48 +0530

    solution commit for day-10

---
+++ 100-days-code/DAY-10/day-10.js
@@ -0,0 +1,5 @@
+// soluton 01
+const convertBinary01 = (numbers) => numbers.map((digit) => digit.toString(2));
+
+const digits = [23, 34, 53, 22, 54, 64, 63, 25, 43];
+console.log(convertBinary01(digits));
