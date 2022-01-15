65bd9a1853d33dedeaa310f82aeefef98bc638aa
Amir Sohel <amirsohel27@gmail.com>
2022-01-15 21:55:00 +0530

    commit solution for day-11

--- 100-days-code/DAY-11/day-11.js
+++ 100-days-code/DAY-11/day-11.js
@@ -1,12 +1,10 @@
 // soluton 01
-const convertBinary01 = (numbers) => {
-  const binaryArray = [];
-  for (let digit of numbers) binaryArray.push(digit.toString(2));
-  return binaryArray;
+const stringToBinary01 = (string) => {
+  let binaryCode = [];
+  for (let index = 0; index < string.length; index++) {
+    binaryCode.push(string[index].charCodeAt().toString(2));
+  }
+  return binaryCode.join(" ");
 };
 
-// soluton 02
-const convertBinary02 = (numbers) => numbers.map((digit) => digit.toString(2));
-
-const digits = [23, 34, 53, 22, 54, 64, 63, 25, 43];
-console.log(convertBinary01(digits));
+console.log(stringToBinary01("Hello World"));
